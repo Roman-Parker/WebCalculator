ff8ed0f129c31be98e72216ba89b3bf1e34c908e
Roman Parker <Roman@parker.pm>
2021-11-24 12:29:16 +0100

    added history

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
 let firstNumber;
 let secondNumber;
+let operator;
 let result;
+let resultArray = [];
+let historyString;
 
 function add(){
+    operator ="+";
     firstNumber = parseInt(document.querySelector("#firstNumber").value);
     secondNumber = parseInt(document.querySelector("#secondNumber").value);
     result = firstNumber + secondNumber;
-
-    document.querySelector("p").innerText = firstNumber + " + " + secondNumber + " = " + result;
+    resultArray.push({ num1: firstNumber , op: operator , num2: secondNumber , answer: " = " + result})
+    printHistory();
+    
 }
 function sub(){
+    operator = "-"
     firstNumber = document.querySelector("#firstNumber").value;
     secondNumber = document.querySelector("#secondNumber").value;
     result = firstNumber - secondNumber;
 
-    document.querySelector("p").innerText = firstNumber + " - " + secondNumber + " = " + result;
+    resultArray.push({ num1: firstNumber , op: operator , num2: secondNumber , answer: " = " + result})
+    printHistory();
 }
 function mult(){
+    operator= "*"
     firstNumber = document.querySelector("#firstNumber").value;
     secondNumber = document.querySelector("#secondNumber").value;
     result = firstNumber * secondNumber;
-
-    document.querySelector("p").innerText = firstNumber + " * " + secondNumber + " = " + result;
+    resultArray.push({ num1: firstNumber , op: operator , num2: secondNumber , answer: " = " + result})
+    printHistory();
 }
 function div(){
+    operator="/";
     firstNumber = document.querySelector("#firstNumber").value;
     secondNumber = document.querySelector("#secondNumber").value;
     if(secondNumber == 0){
@@ -32,5 +41,13 @@ function div(){
     }
     result = firstNumber / secondNumber;
 
-    document.querySelector("p").innerText = firstNumber + " / " + secondNumber + " = " + result;
+    resultArray.push({ num1: firstNumber , op: operator , num2: secondNumber , answer: " = " + result})
+    printHistory();
+}
+function printHistory(){
+    document.querySelector("p").innerText = "";
+    for(i=0; i < resultArray.length;i++){
+        document.querySelector("p").innerText += resultArray[i].num1 + " " + resultArray[i].op + " " +
+         resultArray[i].num2 + " = " + resultArray[i].answer +"\n";
+    }
 }
