9b76ea0cd8aaf38b00ed29689b7c759fb861ee24
Roman Parker <Roman@parker.pm>
2021-11-24 11:21:33 +0100

    added arithmetic  functionallity to calculator

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-function
+let firstNumber;
+let secondNumber;
+let result;
+
+function add(){
+    firstNumber = parseInt(document.querySelector("#firstNumber").value);
+    secondNumber = parseInt(document.querySelector("#secondNumber").value);
+    result = firstNumber + secondNumber;
+
+    document.querySelector("p").innerText = firstNumber + " + " + secondNumber + " = " + result;
+}
+function sub(){
+    firstNumber = document.querySelector("#firstNumber").value;
+    secondNumber = document.querySelector("#secondNumber").value;
+    result = firstNumber - secondNumber;
+
+    document.querySelector("p").innerText = firstNumber + " - " + secondNumber + " = " + result;
+}
+function mult(){
+    firstNumber = document.querySelector("#firstNumber").value;
+    secondNumber = document.querySelector("#secondNumber").value;
+    result = firstNumber * secondNumber;
+
+    document.querySelector("p").innerText = firstNumber + " * " + secondNumber + " = " + result;
+}
+function div(){
+    firstNumber = document.querySelector("#firstNumber").value;
+    secondNumber = document.querySelector("#secondNumber").value;
+    if(secondNumber == 0){
+        alert("Kan inte dividera med noll, skriv ett annat andra tal!")
+        return;
+    }
+    result = firstNumber / secondNumber;
+
+    document.querySelector("p").innerText = firstNumber + " / " + secondNumber + " = " + result;
+}
